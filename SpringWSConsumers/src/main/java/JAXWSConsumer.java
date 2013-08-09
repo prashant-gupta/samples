88619ce3b4fb90c535eec663f1248942d88b0da6
Prashant Gupta <prashant.gupta@middleware360.com>
2013-08-09 20:00:03 +0530

    refactored code for web service consumers

--- SpringWSConsumers/src/main/java/JAXWSConsumer.java
+++ SpringWSConsumers/src/main/java/JAXWSConsumer.java
@@ -1,3 +1,5 @@
+import edu.jft.prashant.interfaces.UtilService;
+import edu.jft.prashant.services.endpoints.UtilServiceEndpoint;
 import edu.jft.prashant.wsendpoints.SpitterServiceEndpoint;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -7,8 +9,17 @@ public class JAXWSConsumer {
     static private ApplicationContext ctx;
 
     public static void main(String... args){
-        SpitterServiceEndpoint spitterService=(SpitterServiceEndpoint)ctx.getBean("spitterService");
-        out.println("============================="+spitterService.echoUsername("prashant"));
+//        UtilService hessianUtilService=(UtilService)ctx.getBean("hessianUtilService");
+//        out.println("=============== "+hessianUtilService.echo("prashant"));
+//
+        UtilService burlapUtilService=(UtilService)ctx.getBean("burlapUtilService");
+        out.println("=============== "+burlapUtilService.echo("prashant"));
+
+        UtilService invUtilService=(UtilService)ctx.getBean("invUtilService");
+        out.println("============================="+invUtilService.echo("gupta"));
+
+        UtilServiceEndpoint utilService=(UtilServiceEndpoint)ctx.getBean("utilService");
+        out.println("============================="+utilService.echo("Hello world !"));
 
     }
 
